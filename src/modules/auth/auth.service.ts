1005af034bed0d49f6697cc4d798169fe539ff91
nutthanon <nutthanon.tho@gmail.com>
2024-04-26 16:56:37 +0700

    feat: add auth module

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import { MailerService } from '@nestjs-modules/mailer'
 import { Injectable } from '@nestjs/common'
+import GenerateForgotPasswordMail from 'src/core/utils/generateForgotPasswordMail'
 import { TimeStampModel } from 'src/models'
+import { IAuthRepository } from 'src/repositories/auth/auth.abstarct'
 import { IUsersRepository } from 'src/repositories/users/users.abstract'
 import { ForgotPasswordDto } from './dto/forgot-password.dto'
 
@@ -8,6 +10,7 @@ import { ForgotPasswordDto } from './dto/forgot-password.dto'
 export class AuthService {
     constructor(
         private readonly usersRepository: IUsersRepository,
+        private readonly authRepository: IAuthRepository,
         private readonly mailService: MailerService,
     ) {}
 
@@ -16,15 +19,17 @@ export class AuthService {
         const user = await this.usersRepository.getByEmail(email, false)
 
         if (user) {
-            try {
+            const credentail = await this.authRepository.createForgotPasswordCredentials(user.id)
+
+            console.table(credentail)
+
+            if (credentail) {
                 await this.mailService.sendMail({
                     to: email,
                     from: process.env.DEFAULT_EMAIL_FROM,
                     subject: 'Forgot Password',
-                    text: `Forgot Password link: www.google.com`,
+                    html: GenerateForgotPasswordMail('www.google.com'),
                 })
-            } catch (error) {
-                console.log(error)
             }
         }
 
---
+++ src/repositories/auth/auth.repository.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common'
+import GenerateMinuteLeft from 'src/core/utils/generateMinuteLeft'
+import { ForgotPasswordCredentialModel } from 'src/models/forgot_password_credential'
+import { PrismaService } from 'src/providers/databases/prisma/prisma.service'
+import { v4 as uuid } from 'uuid'
+import { IAuthRepository } from './auth.abstarct'
+import { AuthFactory } from './auth.factory'
+
+@Injectable()
+export class AuthRepository implements IAuthRepository {
+    constructor(
+        private prisma: PrismaService,
+        private authFactory: AuthFactory,
+    ) {}
+
+    async createForgotPasswordCredentials(user_id: number): Promise<ForgotPasswordCredentialModel> {
+        const expiredDate = GenerateMinuteLeft(5)
+
+        const creds = await this.prisma.forgot_password_credential.create({
+            data: {
+                userId: user_id,
+                password_credential: uuid(),
+                is_active: true,
+                expiredDate: expiredDate,
+                createdBy: -1,
+                createdDate: new Date(),
+            },
+        })
+
+        return this.authFactory.mapForgotPasswordCredentialEntityToForgotPasswordCredentialModel(
+            creds,
+        )
+    }
+}
