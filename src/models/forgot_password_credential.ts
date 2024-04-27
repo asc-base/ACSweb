54ed57b6a105cf0f8a8df2a9c1e32b6d469ffe61
nutthanon <nutthanon.tho@gmail.com>
2024-04-27 23:28:57 +0700

    del: delete files

--- src/models/forgot_password_credential.ts
+++
@@ -1,9 +0,0 @@
-import { BaseModel } from 'src/models'
-
-export interface ForgotPasswordCredentialModel extends BaseModel {
-    id: number
-    passwordCredential: string
-    isActive: boolean
-    userId: number
-    expiredDate: Date
-}
