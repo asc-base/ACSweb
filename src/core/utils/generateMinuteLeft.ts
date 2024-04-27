ee53e0947ab957f7339870514fc434f11d415116
nutthanon <nutthanon.tho@gmail.com>
2024-04-27 23:28:57 +0700

    del: delete files

--- src/core/utils/generateMinuteLeft.ts
+++
@@ -1,7 +0,0 @@
-const GenerateMinuteLeft = (minute: number): Date => {
-    const date = new Date()
-    date.setMinutes(date.getMinutes() + minute)
-    return date
-}
-
-export default GenerateMinuteLeft
