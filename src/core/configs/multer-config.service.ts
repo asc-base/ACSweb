f22f190a9cab4494a7c8ec0a31180a06a57ddcc5
famesensor <sathit.ju@outlook.com>
2024-05-13 22:20:18 +0700

    feat: implement multer config service

---
+++ src/core/configs/multer-config.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common'
+import { MulterModuleOptions, MulterOptionsFactory } from '@nestjs/platform-express'
+import { memoryStorage } from 'multer'
+
+@Injectable()
+export class MulterConfigService implements MulterOptionsFactory {
+    createMulterOptions(): MulterModuleOptions {
+        return {
+            storage: memoryStorage(),
+        }
+    }
+}
