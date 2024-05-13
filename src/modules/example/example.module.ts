b645c2122d8984b61c39281868f8973f3296269d
famesensor <sathit.ju@outlook.com>
2024-05-13 22:21:22 +0700

    feat: implement example upload file

--- src/modules/example/example.module.ts
+++ src/modules/example/example.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common'
 import { ExampleFactory } from 'src/modules/example/example.factory'
 import { ExampleService } from 'src/modules/example/example.service'
+import { FireStorageModule } from 'src/providers/storage/firestorage.module'
 import { RepositoriesModule } from 'src/repositories/repositories.module'
 import { ExampleController } from './example.controller'
 
 @Module({
-    imports: [RepositoriesModule],
+    imports: [RepositoriesModule, FireStorageModule],
     controllers: [ExampleController],
     providers: [ExampleService, ExampleFactory],
 })
--- src/modules/example/example.service.ts
+++ src/modules/example/example.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@nestjs/common'
 import { ExampleModel } from 'src/models/example'
 import { CreateExampleDto } from 'src/modules/example/dto/create-example.dto'
+import { FireStorageService } from 'src/providers/storage/firestorage.service'
 import { IExampleRepository } from 'src/repositories/example/example.abstract'
 
 @Injectable()
 export class ExampleService {
-    constructor(private exampleRepository: IExampleRepository) {}
+    constructor(
+        private exampleRepository: IExampleRepository,
+        private fireStorageService: FireStorageService,
+    ) {}
 
     async createExample({ name }: CreateExampleDto): Promise<ExampleModel> {
         return await this.exampleRepository.create({ name })
@@ -13,5 +17,9 @@ export class ExampleService {
 
     async getExampleById(id: number): Promise<ExampleModel> {
         return await this.exampleRepository.getById(id)
+    }
+
+    async uploadFile(file: Express.Multer.File): Promise<string> {
+        return await this.fireStorageService.upload('example', file)
     }
 }
