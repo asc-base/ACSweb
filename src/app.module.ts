fcb9bd3c4873afa020459493f1ef7d61cc88e9c1
famesensor <sathit.ju@outlook.com>
2024-05-13 22:20:39 +0700

    feat: init module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+import { MulterModule } from '@nestjs/platform-express'
+import { MulterConfigService } from 'src/core/configs/multer-config.service'
 import { PrismaModule } from 'src/providers/databases/prisma/prisma.module'
+import { FireStorageModule } from 'src/providers/storage/firestorage.module'
 import { AuthModule } from './modules/auth/auth.module'
 import { ExampleModule } from './modules/example/example.module'
 import { HealthModule } from './modules/health/health.module'
@@ -16,6 +19,10 @@ import { UsersModule } from './modules/users/users.module'
         PrismaModule,
         UsersModule,
         AuthModule,
+        FireStorageModule,
+        MulterModule.registerAsync({
+            useClass: MulterConfigService,
+        }),
     ],
 })
 export class AppModule {}
