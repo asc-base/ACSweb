b703b2d6fa9617af53c202a893dfc7e085bee96e
NUTTHANON <Nutthanon.tho@gmail.com>
2024-05-22 17:39:33 +0700

    refactor: Guard

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -7,19 +7,19 @@ import {
     Query,
     UseGuards,
 } from '@nestjs/common'
-import { AuthGuard } from '@nestjs/passport'
 import { ApiBearerAuth } from '@nestjs/swagger'
 import { Pageable } from 'src/models'
 import { UsersModel } from 'src/models/users'
 import { UsersDto } from 'src/modules/users/dto/users.dto'
 import { UsersFactory } from 'src/modules/users/users.factory'
 import { GetUser } from 'src/shared/decorators/get-user.decorator'
+import { JwtAuthGuard } from '../auth/guard/auth.guard'
 import { QueryUserDto } from './dto/get-users.dto'
 import { UsersService } from './users.service'
 
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard)
 @Controller('users')
-@UseGuards(AuthGuard('jwt'))
-@ApiBearerAuth('access-token')
 export class UsersController {
     constructor(
         private readonly usersService: UsersService,
