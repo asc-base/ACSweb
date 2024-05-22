ad47c5bd68b714bc5e6e1fcfd4f43cc2ff7fdf84
NUTTHANON <Nutthanon.tho@gmail.com>
2024-05-22 18:27:20 +0700

    fix: import path

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -8,12 +8,12 @@ import {
     UseGuards,
 } from '@nestjs/common'
 import { ApiBearerAuth } from '@nestjs/swagger'
+import { JwtAuthGuard } from 'src/core/guard/auth.guard'
 import { Pageable } from 'src/models'
 import { UsersModel } from 'src/models/users'
 import { UsersDto } from 'src/modules/users/dto/users.dto'
 import { UsersFactory } from 'src/modules/users/users.factory'
 import { GetUser } from 'src/shared/decorators/get-user.decorator'
-import { JwtAuthGuard } from '../../core/guard/auth.guard'
 import { QueryUserDto } from './dto/get-users.dto'
 import { UsersService } from './users.service'
 
